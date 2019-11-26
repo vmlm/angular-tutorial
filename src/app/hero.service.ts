ab999cca04d3d6342603fe947a931f3e692f5cf3
Victor Lara <v.lara@iqcorpgroup.com>
2019-11-26 16:40:16 -0500

    made several changes, got all the way to the http section of the tutorial.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,18 +1,56 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 import { Hero } from './hero';
 import { MessagesService } from './messages.service';
-import { HEROES } from './mock-heroes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messagesService: MessagesService) { }
+	private heroesUrl = 'api/heroes';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Contet-Type': 'application/json' })
+  };
+
+  constructor(
+  	private http: HttpClient,
+  	private messagesService: MessagesService) { }
+
+  private log(message: string) {
+  	this.messagesService.add(`HeroService: ${message}`);
+  }
 
   getHeroes (): Observable<Hero[]> {
-  	this.messagesService.add('HeroService: fetched heroes');
-  	return of(HEROES);
+  	return this.http.get<Hero[]>(this.heroesUrl)
+  		.pipe(
+  			tap(_ => this.log('fetched heroes')),
+  			catchError(this.handleError<Hero[]>('getHeroes', []))
+  		);
+  }
+
+  getHero (id: number): Observable<Hero> {
+  	const url = `${this.heroesUrl}/${id}`;
+  	return this.http.get<Hero>(url).pipe(
+  		tap(_ => this.log (`fetched hero id=${id}`)),
+  		catchError(this.handleError<Hero>(`getHero id=${id}`))
+  	);
+  }
+
+  updateHero (hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+  	return (error: any): Observable <T> => {
+  		console.error(error)
+  		this.log(`${operation} failed: ${error.message}`);
+  		return of(result as T);
+  	};
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,8 +10,6 @@ import { Hero } from '../hero';
 
 export class HeroesComponent implements OnInit {
 	heroes: Hero[];
-	selectedHero: Hero;
-
 
   constructor(private heroService: HeroService) {
   }
@@ -24,9 +22,4 @@ export class HeroesComponent implements OnInit {
   	this.heroService.getHeroes()
   		.subscribe(heroes => this.heroes = heroes);
   }
-
-  onSelect (hero: Hero): void {
-  	this.selectedHero = hero;
-  }
-
 }
